d1bd6ba250707ad9b57219748b36dc2875d69b44
Donny Wu <enderpicture@gmail.com>
2020-10-20 19:10:39 -0700

    Animations added

--- Assets/DishButtonScript.cs
+++ Assets/DishButtonScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class DishButtonScript : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Animator animator;
+    public Animator dishAnimator;
     void Start()
     {
         
@@ -15,8 +17,17 @@ public class DishButtonScript : MonoBehaviour
     {
         
     }
-
-    private void OnCollisionEnter(Collision other) {
-        
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log(other.transform.name);
+        if (other.transform.tag == "Player") {
+            animator.SetBool("goDown", true);
+            dishAnimator.SetBool("goDown", true);
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if (other.transform.tag == "Player") {
+            animator.SetBool("goDown", false);
+            dishAnimator.SetBool("goDown", false);
+        }
     }
 }

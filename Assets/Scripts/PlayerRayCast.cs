b9eee68557e435e45ec9ff0a579a017a4bcc8c2c
Donny Wu <enderpicture@gmail.com>
2020-10-21 15:42:34 -0700

    Probuilder works + ocean sounds

---
+++ Assets/Scripts/PlayerRayCast.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRayCast : MonoBehaviour
+{
+    float range = 10f;
+    float power = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // https://www.youtube.com/watch?v=THnivyG0Mvo
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+        {
+            Rigidbody rb = hit.transform.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(power, transform.position, range);
+            }
+        }
+
+    }
+}

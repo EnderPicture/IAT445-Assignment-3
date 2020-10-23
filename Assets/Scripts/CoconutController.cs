90d2d44da025644008bb59027adcd0d754cf1d92
Donny Wu <enderpicture@gmail.com>
2020-10-22 18:50:02 -0700

    new lightmap + floating coconuts

---
+++ Assets/Scripts/CoconutController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoconutController : MonoBehaviour
+{
+    public float landHeight;
+    public float waterDrag;
+    public float buoyancy;
+    Rigidbody rigidbody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (transform.position.y < landHeight)
+        {
+            rigidbody.AddForce(new Vector3(0,buoyancy,0));
+            rigidbody.drag = waterDrag;
+        }
+        else 
+        {
+            rigidbody.drag = 0;
+        }
+    }
+}
